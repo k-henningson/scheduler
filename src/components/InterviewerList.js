5635389ef4b576857d150d5308fa6ea132c9f14d
Kyra Henningson <kyrahenningson@hotmail.com>
2022-10-04 16:15:10 -0700

    clean up code

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -24,9 +24,3 @@ export default function InterviewerList(props) {
 </section> 
   );
 }
-
-// key={day.id}
-// name={day.name}
-// spots={day.spots}
-// selected={day.name === props.day}
-// setDay={props.setDay}
