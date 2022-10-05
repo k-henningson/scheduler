0deafadb5dd70fa0183e8868d8abbd2d8d65ad6c
Kyra Henningson <kyrahenningson@hotmail.com>
2022-10-05 16:04:36 -0700

    display appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import "components/Appointment/styles.scss"
+import Header from './Header';
+import Show from './Show';
+import Empty from './Empty';
 
 
-export default function Appointment() {
+export default function Appointment(props) {
+  console.log('props', props);
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time}/>
+      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty />}
+    </article>
   );
 }
 
