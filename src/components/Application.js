79049e9ab4d3fd21b51496d6f0fb8b08545f30b7
Kyra Henningson <kyrahenningson@hotmail.com>
2022-10-07 13:48:16 -0700

    retrieve interview data, and errors with axios most recent version release

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 
 export default function Application(props) {
@@ -13,7 +13,8 @@ export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
   const setDay = (day) => {
@@ -47,10 +48,12 @@ export default function Application(props) {
   const dailyAppointments = getAppointmentsForDay(state, state.day);
 
   const schedule = dailyAppointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
     return (
       <Appointment 
         key={appointment.id} 
         {...appointment} 
+        interview={interview}
       />
     );
   });
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -14,3 +14,12 @@ export function getAppointmentsForDay(state, day) {
   }
   return result
 };
+
+export function getInterview(state, interview) {
+  if (!interview) return null;
+  const interviewObj = {
+    student: interview.student
+  };
+  interviewObj.interviewer = state.interviewers[interview.interviewer];
+  return interviewObj;
+};
