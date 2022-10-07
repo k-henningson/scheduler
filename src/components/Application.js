8306e3a24068ffaa320bf9f93e0ef3621701df40
Kyra Henningson <kyrahenningson@hotmail.com>
2022-10-06 17:23:32 -0700

    manage state of day and days

--- src/components/Application.js
+++ src/components/Application.js
@@ -49,8 +49,19 @@ export default function Application(props) {
 
   //State
 
-  const [days, setDays] = useState([]);
-  const [day, setDay] = useState("Monday");
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    // you may put the line below, but will have to remove/comment hardcoded appointments variable
+    //appointments: {}
+  });
+
+  const setDay = day => setState({ ...state, day });
+
+  const setDays = (days) => {
+    setState(prev => ({ ...prev, days }));
+}
+
 
   //Effect
 
@@ -86,8 +97,8 @@ export default function Application(props) {
       <hr className="sidebar__separator sidebar--centered" />
       <nav className="sidebar__menu">
       <DayList
-        days={days}
-        value={day}
+        days={state.days}
+        value={state.day}
         onChange={setDay}
       />
       </nav>
