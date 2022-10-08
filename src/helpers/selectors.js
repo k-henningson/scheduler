5818a3899fcb7ec8a83ba129318d3ea3961543b4
Kyra Henningson <kyrahenningson@hotmail.com>
2022-10-08 11:10:08 -0700

    add an interviewers selector

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -23,3 +23,20 @@ export function getInterview(state, interview) {
   interviewObj.interviewer = state.interviewers[interview.interviewer];
   return interviewObj;
 };
+
+export function getInterviewersForDay(state, day) {
+  //Find provided day from object
+  let specificDay = state.days.filter(d => d.name === day)[0];
+  if(!specificDay) {
+   return [];
+  }
+  let result = [];
+
+  // Find where id matches id of states.appointments
+  for(const id of specificDay.interviewers) {
+    const interviewersObj = state.interviewers[id];
+  
+    result.push(interviewersObj);
+  }
+  return result
+};
