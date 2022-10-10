e8790dec7d6a901cfaef4dce5c97a7fb8a599dd5
Kyra Henningson <kyrahenningson@hotmail.com>
2022-10-10 15:04:03 -0700

    edit appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -16,6 +16,7 @@ export default function Appointment(props) {
   const SAVING = "SAVING";
   const DELETING = "DELETING";
   const CONFIRM = "CONFIRM"
+  const EDIT = "EDIT";
 
 
   const { mode, transition, back } = useVisualMode(
@@ -48,12 +49,14 @@ export default function Appointment(props) {
           student={props.interview.student}
           interviewer={props.interview.interviewer}
           onDelete={() => transition(CONFIRM)}
+          onEdit={() => transition(EDIT)}
         />
       )}
       {mode === CREATE && <Form interviewers={props.interviewers} onCancel={back} onSave={save}/>}
       {mode === SAVING  && <Status message={"Saving"}/>}
       {mode === CONFIRM  && <Confirm message={"Are you sure you would like to delete?"} onConfirm={deleting} onCancel={back}/>}
       {mode === DELETING  && <Status message={"Deleting"}/>}
+      {mode === EDIT && <Form student={props.interview.student} interviewer={props.interview.interviewer.id} interviewers={props.interviewers} onCancel={back} onSave={save} />}
     </article>
   );
 }
