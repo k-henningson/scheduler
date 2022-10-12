cd6b0c2403efafdadd7de140a450e78544269b14
Kyra Henningson <kyrahenningson@hotmail.com>
2022-10-11 17:04:54 -0700

    remove dead code

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -6,16 +6,13 @@ import Application from "components/Application";
 
 afterEach(cleanup);
 
-// it("renders without crashing", () => {
-//   render(<Application />);
-// });
 
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
+it("changes the schedule when a new day is selected", () => {
   const { getByText } = render(<Application />);
 
-  return waitForElement(() => getByText("Monday"))
-  .then(() => {
+  return waitForElement(() => getByText("Monday")).then(() => {
     fireEvent.click(getByText("Tuesday"));
+
     expect(getByText("Leopold Silvers")).toBeInTheDocument();
   });
 });
